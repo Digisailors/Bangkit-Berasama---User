dceb916de4c390011f24b6f96ee55dadad731121
rampo <rajeshsankaravadivell@gmail.com>
2022-01-18 20:05:39 +0530

    feedback

--- lib/constants/themeconstants.dart
+++ lib/constants/themeconstants.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 
 getHeight(BuildContext context) => MediaQuery.of(context).size.height;
 getWidth(BuildContext context) => MediaQuery.of(context).size.width;
-getText(BuildContext context)=> Theme.of(context).textTheme;
+getText(BuildContext context)=> Theme.of(context). textTheme;
 
